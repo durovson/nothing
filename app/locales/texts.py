8c2d329727a6cbf5261fd92cae6f492ae5fb45d0
nothing <nikitacirskov0@gmail.com>
2026-08-27 10:01:01 +0300

    Update texts.py

--- app/locales/texts.py
+++ app/locales/texts.py
@@ -249,7 +249,7 @@ TEXTS: dict[Language, dict[TextKey, str]] = {
         TextKey.REFERRAL_CAPTION: (
             "<tg-emoji emoji-id='5942877472163892475'>👥</tg-emoji> <b>Рефералы</b>\n\n<b>Детали:</b>\n"
             "<blockquote>• <b>Уровень:</b> {level}\n\n"
-            "• <b>Реферальный процент:</b>\n   {rate} % комиссии\n\n"
+            "• <b>Реферальный процент:</b>\n   {rate}% комиссии\n\n"
             "• <b>Реферальный объём:</b>\n   {volume} GRAM\n\n"
             "• <b>Приглашено пользователей:</b> {count}</blockquote>{community_status}\n\n"
             "<b>Баланс:</b>\n"
@@ -538,7 +538,7 @@ TEXTS: dict[Language, dict[TextKey, str]] = {
             "<tg-emoji emoji-id='5942877472163892475'>👥</tg-emoji> <b>Referrals</b>\n\n"
             "<b>Details:</b>\n"
             "<blockquote>• <b>Level:</b> {level}\n\n"
-            "• Referral percentage:\n  {rate} % of the service fee\n\n"
+            "• Referral percentage:\n  {rate}% of the service fee\n\n"
             "• Cumulative volume:\n {volume} GRAM\n\n"
             "• Users invited: {count}</blockquote>{community_status}\n\n"
             "<b>Balance:</b>\n"
